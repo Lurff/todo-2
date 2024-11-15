eebb8798f9c90ae7a5e6196a1b314f141e8aa299
Atakan Çoban <98714395+Lurff@users.noreply.github.com>
2024-11-15 20:47:10 +0300

    Update List.jsx

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -8,7 +8,7 @@ const List = () => {
 	const { value } = useContext(Context);
 
 	return (
-		<div className="w-1/2 h-full overflow-y-auto flex flex-col scroll-smooth">
+		<div className="w-1/2 h-full overflow-y-auto flex flex-col scroll-smooth max-lg:w-3/4">
 			{value.todos.map((todo, index) => (
 				<Todo key={index} index={index} text={todo.text} isDone={todo.isDone} />
 			))}
